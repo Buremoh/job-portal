47678eafe88c5d8b659bfb770b1113b99af7f133
Buremoh <51365028+Buremoh@users.noreply.github.com>
2020-05-08 23:31:14 +0000

    my first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function apply1() {
+  document.location.href = "https://bit.ly/2xJ6WMO";
+}
+
+function apply2() {
+  document.location.href = "https://bit.ly/2SOCrfK";
+}
+
+function apply3() {
+  document.location.href = "https://bit.ly/35Km75d";
+}
+
+function apply4() {
+  document.location.href = "https://bit.ly/3fzjreT";
+}
+
+function apply5() {
+  document.location.href = "https://bit.ly/2WxAxRG";
+}
